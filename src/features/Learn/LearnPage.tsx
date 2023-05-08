6302e7029ee76e119d2656d2bdd99c168cb1e382
LDS196 <lihachevd87@gmail.com>
2023-05-08 20:54:21 +0400

    refactoring

--- src/features/Learn/LearnPage.tsx
+++ src/features/Learn/LearnPage.tsx
@@ -9,23 +9,11 @@ import s from "./LearnPage.module.scss"
 import { selectIsLoading } from "app/app.select"
 import { ControlledRadioButtonsGroup } from "features/Learn/ControlledRadioButtonsGroup"
 import Back from "common/components/Back"
+import { getCard } from "common/utils/get-card-util"
 
 const grades = ["Didn't know", "Forgot", "A lot of thought", "Confused", "Knew the answer"]
 
-const getCard = (cards: CardType[]) => {
-    const sum = cards.reduce((acc, card) => acc + (6 - card.grade) * (6 - card.grade), 0)
-    const rand = Math.random() * sum
-    const res = cards.reduce(
-        (acc: { sum: number; id: number }, card, i) => {
-            const newSum = acc.sum + (6 - card.grade) * (6 - card.grade)
-            return { sum: newSum, id: newSum < rand ? i : acc.id }
-        },
-        { sum: 0, id: -1 }
-    )
-    console.log("test: ", sum, rand, res)
 
-    return cards[res.id + 1]
-}
 
 const LearnPage = () => {
     const isLoading = useSelector(selectIsLoading)

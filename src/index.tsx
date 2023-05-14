774a75ec11c7f5cdcdb1ee62b781d857b1b537a2
LDS196 <lihachevd87@gmail.com>
2023-05-14 19:30:17 +0400

    refactoring

--- src/index.tsx
+++ src/index.tsx
@@ -5,20 +5,20 @@ import { persistor, store } from "app/store"
 import App from "app/App"
 import reportWebVitals from "./reportWebVitals"
 import "./index.css"
-import { HashRouter } from "react-router-dom"
+import { BrowserRouter, HashRouter } from "react-router-dom"
 import { PersistGate } from "redux-persist/integration/react"
 
 const container = document.getElementById("root")!
 const root = createRoot(container)
 
 root.render(
-    <HashRouter>
+    <BrowserRouter>
         <Provider store={store}>
             <PersistGate loading={null} persistor={persistor}>
                 <App />
             </PersistGate>
         </Provider>
-    </HashRouter>
+    </BrowserRouter>
 )
 
 reportWebVitals()
